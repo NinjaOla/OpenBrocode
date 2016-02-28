5e4b381e3901dd5c241ad4c37629a89931b20171
Nyranith <kaalsaas@hotmail.com>
2016-02-28 22:33:19 +0100

    Added cert and scopes.

---
+++ Server/Server/Settings/Cert.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Settings
+{
+    class Cert
+    {
+        public static X509Certificate2 Load()
+        {
+            var assembly = typeof(Cert).Assembly; 
+            using (var )
+        }
+    }
+}
