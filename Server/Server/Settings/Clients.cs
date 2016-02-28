e85e016fc6130e188e6f35523a57b7558ca16c23
Nyranith <kaalsaas@hotmail.com>
2016-02-28 22:33:04 +0100

    Starting on the server using IdentityServer-3

---
+++ Server/Server/Settings/Clients.cs
@@ -0,0 +1,36 @@
+﻿using IdentityServer3.Core;
+using IdentityServer3.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Settings
+{
+
+    // https://www.scottbrady91.com/Identity-Server/Identity-Server-3-Standalone-Implementation-Part-1
+    class Clients
+    {
+        public static IEnumerable<Client> Get() {
+            return new List<Client> {
+                new Client {
+                    ClientId = "1234", 
+                    ClientName = "Example implicit client", 
+                    Enabled = true, 
+                    Flow = Flows.Implicit, 
+                    RequireConsent = true, 
+                    AllowRememberConsent = true, 
+                    RedirectUris = new List<string>(), 
+                    AllowedScopes = new List<string> {
+                        Constants.StandardScopes.OpenId,
+                        Constants.StandardScopes.Profile,
+                        Constants.StandardScopes.Email
+                    },
+                    AccessTokenType = AccessTokenType.Jwt 
+                }
+            };
+
+        }
+    }
+}
---
+++ Server/Server/Startup.cs
@@ -0,0 +1,25 @@
+﻿using IdentityServer3.Core.Configuration;
+using Owin;
+using Server.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Server
+{
+    internal class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            IdentityServerServiceFactory factory = new IdentityServerServiceFactory();
+
+            factory
+                    .UseInMemoryClients(Clients.Get())
+                    .UseInMemoryClients(Scopes.get())
+                    .UseInMemoryClients(Users.get());
+        }
+    }
+}
