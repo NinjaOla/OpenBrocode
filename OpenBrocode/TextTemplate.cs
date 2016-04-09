0be0e8726e207b9743f189970f6051036cbb572a
Nyranith <kaalsaas@hotmail.com>
2016-04-09 17:00:24 +0200

    Working on the server.

---
+++ OpenBrocode/TextTemplate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenBrocode
+{
+    class TextTemplate
+    {
+        public static string buttonText = "This is anohter test"; 
+    }
+}
